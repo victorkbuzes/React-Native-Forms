4922e9f475b3aa31706bf70f9f1553944b72fc31
code <victorkbuzes@gmail.com>
2022-09-08 09:54:01 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Form from './src/featres/Form';
+import { Forms } from './src/featres/Forms';
+
+
+export default function App() {
+  return (
+    <View >
+      <Forms/>
+     
+    </View>
+  );
+}
+
